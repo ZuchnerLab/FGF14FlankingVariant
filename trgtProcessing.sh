e6ad034115f231ae1839d7e0dd9a925bf7c8e800
ZuchnerLab <65570185+ZuchnerLab@users.noreply.github.com>
2024-05-13 15:55:55 -0500

    Update trgtProcessing.sh

--- trgtProcessing.sh
+++ trgtProcessing.sh
@@ -12,10 +12,18 @@ samplePrefix=$2
 ref=$3
 catalog=$4
 
+# download TRGT
 wget https://github.com/PacificBiosciences/trgt/releases/download/v0.3.3/trgt-v0.3.3-linux_x86_64.gz
 gunzip trgt-v0.3.3-linux_x86_64.gz
 mv trgt-v0.3.3-linux_x86_64 trgt
 chmod 700 trgt
 
+# prepare to run segmentation script
+pip install pandas Levenshtein regex
+
+# genotype tandem repeats with TRGT
 ./trgt --genome ${ref} --repeats ${catalog} --reads ${inputBam} --threads $(nproc) --output-prefix ${samplePrefix}
+
+# parse TRGT output specifically for the FGF14 locus
+python segmentTRGTAlleles.py --sample=${samplePrefix}
 
